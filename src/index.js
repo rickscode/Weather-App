7bdbff8606ad7fd1d3cb164af4c364829cd6f22b
“Ricky <grickyh@gmail.com>
2021-06-02 11:18:24 +0100

    search box weather location

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-async function getWeather() {
-  const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=lodon&units=metric&appid=1f2ee12c8f8994c48f986ab8a9acd1d7', {mode: 'cors'})
+// Global bindings
+const searchButton = document.getElementById('submit-search');
+
+
+// Get current weather of location
+async function getWeather(location) {
+  const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + location + '&units=metric&appid=1f2ee12c8f8994c48f986ab8a9acd1d7', {mode: 'cors'})
   if (response.status === 404) {
    console.log('error');
  } else {
@@ -9,4 +14,8 @@ async function getWeather() {
 }
  
 
-getWeather()
+// Send location
+searchButton.addEventListener('click', function(){
+  let location = document.getElementById('location-search-input').value;
+  getWeather(location)
+});
